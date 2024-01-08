fd7bbb9331bdb872ff6339bbd1c7153860c3a3cf
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-01-09 00:30:58 +0300

    refactor(advanced-1.2): add media alternatives

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="btn btn_delete";
     deleteButtonImg.src="./remove.svg";
+    deleteButtonImg.alt = 'remove';
     deleteButton.appendChild(deleteButtonImg);
 
 
